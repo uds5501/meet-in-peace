871bb8046ae624849288513d0f0bdaf9dea5cadb
Logan-47 <ashishofficial47@gmail.com>
2021-04-03 10:33:12 +0530

    feat: added slack support

--- src/background.js
+++ src/background.js
@@ -1,3 +1,12 @@
+const gmeet_url = "https://meet.google.com/*";
+const loadMeet_url = "/loadMeet.js";
+
+const zoom_url = "https://zoom.us/*";
+const loadZoom_url = "/loadZoom.js";
+
+const slack_url = "https://app.slack.com/*";
+const loadSlack_url = "/loadSlack.js";
+
 chrome.runtime.onInstalled.addListener(function (details) {
     chrome.storage.sync.get([
         'mainButton',
@@ -5,31 +14,39 @@ chrome.runtime.onInstalled.addListener(function (details) {
         'gmeet_participants',
         'gmeet_video',
         'gmeet_entry',
+        'gmeet_badge',
         'zoom_messages',
         'zoom_participants',
         'zoom_video',
         // 'zoom_entry',
         'general_messages',
-        'gmeet_badge',
+        'slack_messages',
+        'slack_sidebar',
+        'slack_members',
+        'slack_workspace',
+        'slack_userProfile'
     ], function (data) {
         data.mainButton == null && chrome.storage.sync.set({ mainButton: true });
+        
         data.gmeet_messages == null && chrome.storage.sync.set({ gmeet_messages: true });
         data.gmeet_participants == null && chrome.storage.sync.set({ gmeet_participants: true });
         data.gmeet_video == null && chrome.storage.sync.set({ gmeet_video: false });
         data.gmeet_entry == null && chrome.storage.sync.set({ gmeet_entry: false });
+        data.gmeet_badge == null && chrome.storage.sync.set({ gmeet_badge: false });
+        
         data.zoom_messages == null && chrome.storage.sync.set({ zoom_messages: true });
         data.zoom_video == null && chrome.storage.sync.set({ zoom_video: false });
         // data.zoom_entry == null && chrome.storage.sync.set({ zoom_entry: false });
+
+        data.slack_messages == null && chrome.storage.sync.set({ slack_messages: true });
+        data.slack_members == null && chrome.storage.sync.set({ slack_messages: true });
+        data.slack_sidebar == null && chrome.storage.sync.set({ slack_sidebar: false });
+        data.slack_workspace == null && chrome.storage.sync.set({ slack_workspace: false });
+        data.slack_userProfile == null && chrome.storage.sync.set({ slack_userProfile: false });
+
         data.general_messages == null && chrome.storage.sync.set({ general_messages: false });
-        data.gmeet_badge == null && chrome.storage.sync.set({ gmeet_badge: false });
     });
 });
-
-const gmeet_url = "https://meet.google.com/*";
-const loadMeet_url = "/loadMeet.js";
-
-const zoom_url = "https://zoom.us/*";
-const loadZoom_url = "/loadZoom.js";
 
 chrome.commands.onCommand.addListener(function (command) {
     if (command == 'toggle') {
@@ -43,6 +60,10 @@ chrome.commands.onCommand.addListener(function (command) {
             chrome.tabs.query({ url: zoom_url }, function (tabs) {
                 if (tabs.length !== 0)
                     tabs.forEach(function (tab) { chrome.tabs.executeScript(tab.id, { file: loadZoom_url }) });
+            });
+            chrome.tabs.query({ url: slack_url }, function (tabs) {
+                if (tabs.length !== 0)
+                    tabs.forEach(function (tab) { chrome.tabs.executeScript(tab.id, { file: loadSlack_url }) });
             });
             // add others here too
         });
